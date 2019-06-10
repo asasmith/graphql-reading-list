ae549a11156580779194ffdde8adea349840bbb6
Asa Smith <smith.asa.la@gmail.com>
2019-06-09 21:53:47 -0400

    add cors module

--- server/app.js
+++ server/app.js
@@ -2,12 +2,16 @@ const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const schema = require('./schema/schema')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
-mongoose.connect(`mongodb+srv://asa-admin:talss1980Asa@graphqldemo-e6fkb.mongodb.net/test?retryWrites=true&w=majority`, { useNewUrlParser: true })
-mongoose.connection.once('open', () => console.log('connected to mongodb'))
 
 const app = express()
 const PORT = 1111
+
+app.use(cors())
+
+mongoose.connect(`mongodb+srv://asa-admin:talss1980Asa@graphqldemo-e6fkb.mongodb.net/test?retryWrites=true&w=majority`, { useNewUrlParser: true })
+mongoose.connection.once('open', () => console.log('connected to mongodb'))
 
 app.use('/graphql', graphqlHTTP({
   schema,
